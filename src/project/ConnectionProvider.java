accd93ee26b474fe0f4eec9b85f4bc63ab3a6b2d
Commander Bittu <shashankkharga@gmail.com>
2024-03-31 20:51:41 +0530

    Initial commit

---
+++ src/project/ConnectionProvider.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project;
+import java.sql.*;
+/**
+ *
+ * @author shash
+ */
+public class ConnectionProvider {
+    public static Connection getCon()
+    {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/project","root","123456");      
+            return con;
+        }
+        catch(ClassNotFoundException | SQLException e)
+        {
+            return null;
+        }
+    }
+}
